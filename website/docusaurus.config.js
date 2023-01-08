1fdccd29b165d5d5b1fb5788b5a3fcfa662ec171
FreakyCoder <kurayogun@gmail.com>
2023-01-08 21:27:12 +0300

    docs: deploying

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "LulinJS",
   tagline: "LulinJS",
-  url: "https://wrathchaos.github.io/lulinjs/",
+  url: "https://wrathchaos.github.io/lulinjs",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
